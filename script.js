01add5af5e8000a94bf84c054aba36b543297cb6
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-31 12:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your script here.
+let text=document.getElementById("text");
+let utterance;
+document.getElementById("speak").addEventListener('click',function(){
+	let sent=text.value.trim();
+	if(sent !== ''){
+		window.speechSynthesis.cancel();
+		utterance=new SpeechSynthesisUtterance(sent);
+		window.speechSynthesis.speak(utterance);
+	}
+});
+document.getElementById("stop").addEventListener('click', () => {
+      window.speechSynthesis.cancel();
+    });
